613b71c9d3ceaac594a8ed94fea002331aa0abc2
cleverranger <cleverranger@gmail.com>
2022-03-17 10:54:01 +0530

    Google Java Style Guide updates

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -2,6 +2,7 @@ package core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -18,9 +19,7 @@ import java.util.Date;
 
 public class BaseTest {
 
-    //public static final String USER_DIR = System.getProperty("user.dir") + "/drivers/chromedriver.exe";
-    //public static final String USER_DIR = System.getProperty("/drivers/chromedriver.exe");
-    public static final String USER_DIR = "D:\\Test Automation\\Upskilling\\TestAutomationAssessment";
+    public static final String USER_DIR = System.getProperty("user.dir");
     protected final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     @BeforeMethod
@@ -31,13 +30,13 @@ public class BaseTest {
         driver.set(chromeDriver);
     }
 
-    protected WebDriver getWebDriver() {
-        return driver.get();
-    }
-
     protected void openIceHrmAppInWeb() {
         final WebDriver webDriver = getWebDriver();
         webDriver.get(ConfigService.getInstance().getURL());
+    }
+
+    protected WebDriver getWebDriver() {
+        return driver.get();
     }
 
     @AfterMethod
@@ -52,10 +51,11 @@ public class BaseTest {
         TakesScreenshot takesScreenshot = (TakesScreenshot) getWebDriver();
         File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
         final Date currentDate = new Date();
-        final String timeStamp = new SimpleDateFormat("dd-MM-yyyy_hh-mm-ss").format(currentDate);
+        final String timeStamp = new SimpleDateFormat("dd-MM-yyyy_hh-mm-ss")
+            .format(currentDate);
         final String testName = testResult.getName() + " - " + timeStamp;
-        //String fileWithPath = MessageFormat.format("{0}/target/test-results/{1}.png", USER_DIR, testName);
-        String fileWithPath = MessageFormat.format("{0}/target/test-results/{1}.png",testName);
+        String fileWithPath =
+            MessageFormat.format("{0}/target/test-results/{1}.png", USER_DIR, testName);
         File destFile = new File(fileWithPath);
         FileUtils.copyFile(srcFile, destFile);
     }
--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProfilePage extends PageObject {
-    By profileName = By.xpath("//*[@id=\"Employee\"]/div[1]/div/div/div[2]/div/div[2]/div/div[1]/h4");
+    By profileName = By.xpath("//*[@id=\"Employee\"]"
+        + "/div[1]/div/div/div[2]/div/div[2]/div/div[1]/h4");
 
     public ProfilePage(WebDriver driver){
         super(driver);
